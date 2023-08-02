ec1c213a8f725070502404bb59e4505a21a8cf73
dkeesey <460435+dkeesey@users.noreply.github.com>
2023-08-01 22:06:01 -0700

    adding logos and profile pic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,23 @@
-//this only works with Node 14
-
 import Header from '../components/header'
-import ExtLink from '../components/ext-link'
-// import Features from '../components/features'
 import sharedStyles from '../styles/shared.module.css'
+import Image from 'next/image'
+import profilePic from '../../public/dk.jpg'
+import styles from './Home.module.css'
 
 export default function Index() {
   return (
     <>
       <Header titlePre="Home" />
       <div className={sharedStyles.layout}>
+        <div className={styles.profilePicWrapper}>
+          <Image
+            className={styles.profilePic}
+            src={profilePic}
+            alt="Picture of Dean A. Keesey, UI Engineer"
+            width={300}
+            height={300}
+          />
+        </div>
         <h1>Dean A. Keesey</h1>
         <h2>
           Full-stack JavaScript & UI Engineer |<br />
@@ -17,18 +25,19 @@ export default function Index() {
         </h2>
 
         {/* <Features /> */}
+        {/* <img
+          src="/dk.jpg"
+          width="300"
+          height="300"
+          alt="Picture of Dean A. Keesey, UI Engineer"
+        /> */}
 
         <div className="explanation">
           <p>
-            Welcome! I'm Dean, a seasoned engineer with 15+ years of experience
-            delivering web and application design, driving digital
+            Welcome! I'm Dean Keesey, a seasoned engineer with 15+ years of
+            experience delivering web and application design, driving digital
             transformations for Fortune 500 corporations and non-profit
             organizations alike.
-          </p>
-          <p>
-            [Hero Image Suggestion: A high-resolution, professional photo of
-            yourself. An alternative could be an image that represents your work
-            – abstract code lines or an engaging UI design.]
           </p>
           <p>
             Through leveraging my vast UI/UX engineering expertise and a
@@ -41,7 +50,8 @@ export default function Index() {
             Foundation, worked on large-scale client websites at Agency.com, and
             delivered complex licensing contracts at Oracle Corporation. I've
             also designed 3D motion graphics for ESPN's international coverage
-            and resolved critical defects in e-commerce applications.
+            and built features and resolved critical defects in enterprise SaaS
+            applications.
           </p>
           <p>
             [Image Suggestions: Images/logos from the Masumi Hayashi Foundation,
